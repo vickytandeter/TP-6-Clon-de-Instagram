7ed400e7edf33384850dc483e18f7074751561ab
Android Studio <devandroid@ort>
2026-06-16 14:18:37 -0300

    a

--- ClonIg/src/components/Searchbar.jsx
+++ ClonIg/src/components/Searchbar.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 import { useState } from "react";
 
-function SearchBar({ onSearch }) {
+function SearchBar() {
   const [input, setInput] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSearch(input);
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Buscar publicacion..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-    </form>
   );
 }
 
